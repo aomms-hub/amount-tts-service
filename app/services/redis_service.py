df0006b1f31f4d8aa51cafaf78d19027217d09b9
aomms-hub <62050154@kmitl.ac.th>
2025-08-02 00:22:01 +0700

    [improvement] move val to constant

--- app/services/redis_service.py
+++ app/services/redis_service.py
@@ -2,13 +2,14 @@ from app.client.redis_client import redis_client
 
 ttl_seconds = 60 * 60
 
+VOICE_AMOUNT_REDIS_KEY = "voice:amount:"
 def cache_voice(text: str, audio_url: str):
-    key = f"voice:amount:{text}"
+    key = f"{VOICE_AMOUNT_REDIS_KEY}{text}"
     redis_client.set(key, audio_url, ex=ttl_seconds)
 
 
 def get_cached_voice(text: str):
-    key = f"voice:amount:{text}"
+    key = f"{VOICE_AMOUNT_REDIS_KEY}{text}"
     value = redis_client.get(key)
     if value is not None:
         redis_client.expire(key, ttl_seconds)
