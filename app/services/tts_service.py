2889ad690bbe1fc0c95a55a5a1d0f0b04cb10114
aomms-hub <62050154@kmitl.ac.th>
2025-08-01 20:02:25 +0700

    [improvement] implement satang to message

--- app/services/tts_service.py
+++ app/services/tts_service.py
@@ -6,13 +6,17 @@ TTS_PREFIX = "tts"
 
 
 def generate_tts(amount: str, lang: str = "th") -> str:
-    filename = generate_filename(TTS_PREFIX, amount)
+    filename = generate_filename(TTS_PREFIX, amount.replace('.',''))
     tts = gTTS(text=generate_message(amount), lang=lang)
     output_path = os.path.join("static", filename)
     os.makedirs("static", exist_ok=True)
     tts.save(output_path)
     return output_path
 
-
 def generate_message(amount: str) -> str:
-    return f"มีเงินเข้า {amount} บาท"
+    whole, fraction = amount.split(".")
+    fraction = fraction.lstrip("0")
+    message = f"มีเงินเข้า {whole} บาท"
+    if fraction != "00":
+        message += f"{fraction} สตางค์"
+    return message
