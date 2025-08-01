312533d7b7a619a4c47ee8c7dbdd0f01cd8119c6
aomms-hub <62050154@kmitl.ac.th>
2025-08-02 00:22:12 +0700

    [improvement] separate config

--- app/client/redis_client.py
+++ app/client/redis_client.py
@@ -1,9 +1,4 @@
-import os
-from dotenv import load_dotenv
 import redis
-
-load_dotenv()
-
-REDIS_URL = os.getenv("UPSTASH_REDIS_REST_URL")
+from config import REDIS_URL
 
 redis_client = redis.from_url(REDIS_URL, decode_responses=True)
